08b2c13dc9969694694cb165b933d77ae6a86adb
Vignesh Jeyaraj <vignesh.jeyaraj@nike.com>
2020-03-02 15:50:46 -0800

    Added the camel dependencies

--- learning-api/src/main/java/org/spring/learning/camel/CamelConfig.java
+++ learning-api/src/main/java/org/spring/learning/camel/CamelConfig.java
@@ -1,4 +1,25 @@
 package org.spring.learning.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CamelConfig {
+
+    @Bean
+    CamelContextConfiguration contextConfiguration() {
+        return new CamelContextConfiguration() {
+            @Override
+            public void beforeApplicationStart(CamelContext context) {
+                // your custom configuration goes here
+            }
+
+            @Override
+            public void afterApplicationStart(CamelContext camelContext) {
+
+            }
+        };
+    }
 }
